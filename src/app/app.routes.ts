e9f96523246c402b5c5dc55d610759894b407280
Washington Ribeiro da Silva <washinuchiha@gmail.com>
2024-05-10 11:23:28 -0300

    feat: implementation of lazy loading on the music component route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'music',
+        loadComponent: () => import('./pages/music/music.component').then(m => m.MusicComponent),
+    },
+]
